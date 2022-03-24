9e1abdd6e52b262d63d3dd34145ddf8c36d510d2
Thomas Ayih-Akakpo <thomas.ayih-akakpo@acipenser.mg>
2022-03-24 23:16:15 +0300

    validate each chunk upload

--- fileStore/static/fileStore/js/script.js
+++ fileStore/static/fileStore/js/script.js
@@ -14,22 +14,32 @@ class FileUpload {
               </div>
             </div>
           </div>`
-	document.getElementById('uploaded_files').innerHTML = progress;
-	this.file = this.input.files[0];
-	this.upload_chunk(0, null);
+	if (this.input.files.length == 0) {
+	    alert('Please provide a file');
+	} else {
+	    document.getElementById('uploaded_files').innerHTML = progress;
+	    this.file = this.input.files[0];
+	    this.upload_chunk(0, null);
+	}
     }
 
     upload_chunk(start, path) {
 	var form = new FormData();
+	var self = this;
 	var filepath = path;
-	var nextChunk = start + this.max_length + 1;
+	var nextChunk = start + this.chunk_size + 1;
 	var currentChunk = this.file.slice(start, nextChunk);
 	var uploadedChunk = start + currentChunk.size;
-
+	if (uploadedChunk >= this.file.size) {
+	    var stop = 1;
+	} else {
+	    var stop = 0;
+	}
 	form.append('filename', this.file.name);
 	form.append('file', currentChunk);
 	form.append('filepath', filepath);
 	form.append('nextchunk', nextChunk);
+	form.append('stop', stop);
 	$('.filename').text(this.file.name);
 	$('.textbox').text("Uploading file...");
 
@@ -44,10 +54,10 @@ class FileUpload {
                 var xhr = new XMLHttpRequest();
                 xhr.upload.addEventListener('progress', function (e) {
                     if (e.lengthComputable) {
-                        if (this.file.size < this.max_length) {
+                        if (self.file.size < self.max_length) {
                             var percent = Math.round((e.loaded / e.total) * 100);
                         } else {
-                            var percent = Math.round((uploadedChunk / this.file.size) * 100);
+                            var percent = Math.round((uploadedChunk / self.file.size) * 100);
                         }
                         $('.progress-bar').css('width', percent + '%')
                         $('.progress-bar').text(percent + '%')
@@ -67,9 +77,9 @@ class FileUpload {
                 alert(xhr.statusText);
             },
             success: function (res) {
-                if (nextchunk < this.file.size) {
+                if (nextChunk < self.file.size) {
                     filepath = res.filepath
-                    this.upload_file(nextchunk, filepath);
+                    self.upload_chunk(nextChunk, filepath);
                 } else {
                     $('.textbox').text(res.message);
                     alert(res.message)
--- fileStore/views.py
+++ fileStore/views.py
@@ -12,14 +12,21 @@ class FileUpload(View):
     def post(self, request, *args, **kwargs):
         filename = request.POST['filename']
         filepath = request.POST['filepath']
+        nextchunk = request.POST['nextchunk']
+        stop = request.POST['stop']
         csv_file = request.FILES['file'].read()
 
-        # if one parameter is missing
-        # consider it an invalid request
-
-        if not (filename and filepath and csv_file):
-            return JsonResponse({'message': 'Invalid request'})
-        else:
+        if filepath == 'null':  # first chunk
             with open(f'media/{filename}', 'wb+') as f:
                 f.write(csv_file)
-            return JsonResponse({'message': 'Upload successfull'})
+            if int(stop):
+                return JsonResponse({'message': 'Upload successfull'})
+            return JsonResponse({'message': filename})
+        else:
+            path = f'media/{filepath}'
+            with open(f'media/{filename}', 'ab+') as f:
+                f.write(csv_file)
+            if int(stop):
+                return JsonResponse({'message': 'Upload successfull'})
+            return JsonResponse({'message': filename})
+            
