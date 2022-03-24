6bf4b6401ab87170f74224e18a55a9abd10e7fec
Thomas Ayih-Akakpo <thomas.ayih-akakpo@acipenser.mg>
2022-03-25 00:00:07 +0300

    adding a model to store products

--- fileStore/models.py
+++ fileStore/models.py
@@ -4,3 +4,10 @@ class File(models.Model):
     path = models.CharField(unique=True, max_length=100)
     name = models.CharField(max_length=50)
     complete = models.BooleanField(default=False)
+
+
+class Product(models.Model):
+    sku = models.CharField(unique=True, max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    active = models.BooleanField(default=False)
