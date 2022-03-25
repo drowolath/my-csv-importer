57648f0a7ff18d56460b35ca1863730b158ddeb6
Thomas Ayih-Akakpo <thomas.ayih-akakpo@acipenser.mg>
2022-03-25 08:12:26 +0300

    typo

--- csvImporter/celery.py
+++ csvImporter/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csvImporter.settings')
-app = Celery('csvimport')
+app = Celery('csvImporter')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
