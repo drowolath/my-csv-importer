b374263fa366134b728a4af4d8c47ab11f313e8a
Thomas Ayih-Akakpo <thomas.ayih-akakpo@acipenser.mg>
2022-03-25 12:28:56 +0300

    release candidate ready

--- fileStore/tasks.py
+++ fileStore/tasks.py
@@ -5,24 +5,34 @@ import pandas
 import json
 
 @celery_app.task(name="filestore.storeproduct", queue="products")
+def process(data):
+    for item, sku in data['sku'].items():
+        sku = sku.lower()
+        name = data['name'][item]
+        description = data['description'][item]
+        active = bool(
+            int(data.get('active', False)[item])
+        )
+        product, created = Product.objects.get_or_create(sku=sku)
+        if created:
+            product.name = name
+            product.description = description
+            product.active = active
+            product.save()
+        else:
+            Product.objects.filter(sku=product.sku).update(
+                name=name,
+                description=description,
+                active=active
+            )
+
+
+@celery_app.task(name="filestore.slice_csv", queue="slicer")
 def slice_csv(path):
-    print(path)
     df = pandas.read_csv(path)
     limit = 0
     while limit <= df.shape[0]:
         data = df[limit:limit+100].to_json()
         limit += 100
         data = json.loads(data)
-        for item, sku in data['sku'].items():
-            print(item, sku)
-            sku = sku.lower()
-            product, created = Product.objects.get_or_create(sku=sku)
-            if created:
-                product.name = data['name'][item]
-                product.description = data['description'][item]
-                product.save()
-            else:
-                Product.objects.filter(sku=product.sku).update(
-                    name=data['name'][item],
-                    description=data['description'][item]
-                )
+        process.delay(data)
--- fileStore/views.py
+++ fileStore/views.py
@@ -9,6 +9,14 @@ import pandas
 class FileUpload(View):
     template_name = "fileStore/upload.html"
 
+    def close_file(self, stop, path):
+        if int(stop):
+            tasks.slice_csv.delay(path)
+            return JsonResponse(
+                {'message': 'Uploaded successfully', 'filepath': path}
+            )
+        return JsonResponse({'filepath': path})
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {})
 
@@ -17,40 +25,29 @@ class FileUpload(View):
         filepath = request.POST['filepath']
         nextchunk = request.POST['nextchunk']
         stop = request.POST['stop']
-        csv_file = request.FILES['file']
+        csv_file = request.FILES['file'].read()
+
+        path = f'media/{filename}'
 
         if filepath == 'null':  # first chunk
-            path = f'media/{filename}'
             with open(path, 'wb+') as f:
-                f.write(csv_file.read())
+                f.write(csv_file)
             obj, _ = models.File.objects.get_or_create(
                 path=path,
                 name=filename
             )
             obj.complete = bool(int(stop))
             obj.save()
-            if int(stop):
-                tasks.slice_csv.delay(path)
-                print(obj.__dict__)
-                return JsonResponse(
-                    {'message': 'Uploaded successfully', 'filepath': path}
-                )
-            return JsonResponse({'filepath': filename})
+            return self.close_file(stop, path)
         else:
             path = f'media/{filename}'
             obj = models.File.objects.get(path=path)
             if not obj.complete:
                 with open(path, 'ab+') as f:
-                    f.write(csv_file.read())
-                if int(stop):
-                    obj.complete = True
-                    obj.save()
-                    print(obj.__dict__)
-                    tasks.slice_csv.delay(path)
-                    return JsonResponse(
-                        {'message': 'Uploaded successfully', 'filepath': obj.path}
-                    )
-                return JsonResponse({'filepath': obj.path})
+                    f.write(csv_file)
+                obj.complete = bool(int(stop))
+                obj.save()
+                return self.close_file(stop, path)
             else:
                 return JsonResponse(
                     {"message": "File is already complete"}
